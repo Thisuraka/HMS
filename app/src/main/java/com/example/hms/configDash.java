ee9b15e9dba19d24b53481966cafe1809c3a69b3
Thisuraka <64680083+Thisuraka@users.noreply.github.com>
2020-08-15 04:37:55 +0530

    Stage 1

--- app/src/main/java/com/example/hms/configDash.java
+++ app/src/main/java/com/example/hms/configDash.java
@@ -2,7 +2,10 @@ package com.example.hms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class configDash extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class configDash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_dash);
+
+        Button btn1 = (Button)findViewById(R.id.confDashBtn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(configDash.this, updateMenu.class));
+            }
+        });
+        Button btn2 = (Button)findViewById(R.id.confDashBtn2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(configDash.this, orderSuccess.class));
+            }
+        });
     }
 }
