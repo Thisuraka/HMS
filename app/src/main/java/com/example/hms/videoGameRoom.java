bc4f1195b045ec04ebc2fbd054241f386e7850c9
ANIMUS <theekshanadilum@gmail.com>
2020-08-17 02:56:57 +0530

    Teeka commit one

---
+++ app/src/main/java/com/example/hms/videoGameRoom.java
@@ -0,0 +1,14 @@
+package com.example.hms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class videoGameRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_game_room);
+    }
+}
