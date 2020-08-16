3d54fd8545c1471b5821f584d65712d6fad3fae5
hp <udarasamarakoons@gmail.com>
2020-08-16 21:55:31 +0530

    udara one

---
+++ app/src/main/java/com/example/hms/accomMain.java
@@ -0,0 +1,14 @@
+package com.example.hms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class accomMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accom_main);
+    }
+}
