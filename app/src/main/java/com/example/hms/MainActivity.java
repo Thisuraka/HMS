ddc2561f162333b414a0e8a0520fac9c29dcb85d
Thisuraka <64680083+Thisuraka@users.noreply.github.com>
2020-09-29 09:44:07 +0530

    loggedin

--- app/src/main/java/com/example/hms/MainActivity.java
+++ app/src/main/java/com/example/hms/MainActivity.java
@@ -4,24 +4,71 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.hms.classes.User;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtBxWelcomeNic,txtBxWelcomeCode;
+    Button loginBtn;
+    DatabaseReference dbRef;
+    User user;
+
+    private void clearControls(){
+        txtBxWelcomeNic.setText("");
+        txtBxWelcomeCode.setText("");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        user = new User();
+        txtBxWelcomeNic = findViewById(R.id.txtBxWelcomeNic);
+        txtBxWelcomeCode = findViewById(R.id.txtBxWelcomeCode);
+        loginBtn = (Button)findViewById(R.id.loginBtn);
 
-        Button btn = (Button)findViewById(R.id.loginBtn);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, mainPage.class));
-            }
+                dbRef = FirebaseDatabase.getInstance().getReference().child("User");
+                try {
+                    if (TextUtils.isEmpty(txtBxWelcomeNic.getText().toString())){
+                        Toast.makeText(getApplicationContext(), "Please enter NIC", Toast.LENGTH_SHORT).show();}
+                    else if (TextUtils.isEmpty(txtBxWelcomeCode.getText().toString())){
+                        Toast.makeText(getApplicationContext(), "Please enter Code", Toast.LENGTH_SHORT).show();}
+                    else if (txtBxWelcomeNic.equals("admin") && txtBxWelcomeCode.equals("1234")){
+                        Toast.makeText(getApplicationContext(), "Welcome", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this, mainPage.class));}
+                    else {
+                        user.setNic(txtBxWelcomeNic.getText().toString().trim());
+                        user.setCode(Integer.parseInt(txtBxWelcomeCode.getText().toString().trim()));
+
+                        dbRef.child("User").setValue(user);
+
+                        Toast.makeText(getApplicationContext(), "Data Saved", Toast.LENGTH_SHORT).show();
+                        clearControls();
+                        startActivity(new Intent(MainActivity.this, mainPage.class));
+                    }
+
+
+                }
+                catch (NumberFormatException e){
+                    Toast.makeText(getApplicationContext(), "Invalid Contact Number", Toast.LENGTH_SHORT).show();
+                }
+}
         });
     }
+
+
+
 }
