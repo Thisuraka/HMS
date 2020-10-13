27c8acf9b04e57223d5906d727042ab61dbec938
hp <udarasamarakoons@gmail.com>
2020-10-13 12:58:00 +0530

    stage 1.5

---
+++ app/src/main/java/com/example/hms/roomreserve3.java
@@ -0,0 +1,14 @@
+package com.example.hms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class roomreserve3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roomreserve3);
+    }
+}
---
+++ app/src/main/java/com/example/hms/roomreserve4.java
@@ -0,0 +1,14 @@
+package com.example.hms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class roomreserve4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roomreserve4);
+    }
+}
